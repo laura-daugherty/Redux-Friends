e7729b21a9cc958d5148d059c2d3d9528acff698
Laura Daugherty <lauradaugherty6@gmail.com>
2019-07-18 16:00:49 -0700

    Connecting Pages -importing dependancies -creating basic components -importing/exporting components so they render -initial styling -setting up first routes/links

--- my-friends/src/App.js
+++ my-friends/src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from "react-dom";
+import {
+  BrowserRouter,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
+import axios from "axios";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+
+import "./App.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ padding: 30 }}>
+      <div>
+        <Link to="/">Login</Link>
+        <Link to="/profile">Profile</Link>
+      </div>
+      <Route 
+        exact path="/" 
+        component={Login} 
+      />
+      <Route 
+        exact path="/profile"
+        render={Profile}
+      />
+      <h1>This is an App</h1>
     </div>
   );
 }
---
+++ my-friends/src/components/Login.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import * as Yup from "yup"
+import axios from "axios"
+import { redirect } from "react-router-dom"
+
+function Login() {
+
+  return (
+    <div>
+      You should log in.
+    </div>
+  )
+}
+
+export default Login
